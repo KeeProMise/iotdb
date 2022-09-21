a5e47dd0589bc7a9366332eebd2bababfc095d8c
KeePromMise <zhangjian@iie.ac.cn>
2022-09-21 09:49:25 +0800

    add TagSchemaDescriptor

--- server/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
+++ server/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
@@ -857,8 +857,6 @@ public class IoTDBConfig {
    */
   private boolean enableIDTableLogFile = false;
 
-  private int numOfDeviceIdsInMemTable = 65536;
-
   /** whether to use persistent schema mode */
   private String schemaEngineMode = "Memory";
 
@@ -2834,14 +2832,6 @@ public class IoTDBConfig {
 
   public void setEnableIDTableLogFile(boolean enableIDTableLogFile) {
     this.enableIDTableLogFile = enableIDTableLogFile;
-  }
-
-  public int getNumOfDeviceIdsInMemTable() {
-    return numOfDeviceIdsInMemTable;
-  }
-
-  public void setNumOfDeviceIdsInMemTable(int numOfDeviceIdsInMemTable) {
-    this.numOfDeviceIdsInMemTable = numOfDeviceIdsInMemTable;
   }
 
   public String getSchemaEngineMode() {
